44b5d610535931c6ab175e8fe8d291485eece626
BuildTools <thies2003@gmail.com>
2020-12-01 18:40:58 +0100

    (:

--- 01/index.js
+++ 01/index.js
@@ -5,16 +5,14 @@
 // 2 ms - answer: 703131
 
 const part1 = (input) => {
-  let start = new Date()
-  let data = input.split("\n").map(Number);
-
+  const start = new Date()
+  const data = input.split("\n").map(Number);
   for (let i = 0; i < data.length; i++) {
     const element = data[i];
     for (let j = 0; j < data.length; j++) {
       const element1 = data[j];
       if (element1 + element === 2020) {
-        var end = new Date() - start;
-        console.log('Execution time: %dms', end);
+        console.log('Execution time: %dms', new Date() - start);
         return element * element1;
       }
     }
@@ -33,8 +31,7 @@ const part2 = (input) => {
     for (let j = 0; j < data.length; j++) {
       for (let k = 0; k < data.length; k++) {
         if (data[i] + data[j] + data[k] === 2020) {
-          var end = new Date() - start;
-          console.log('Execution time: %dms', end);
+          console.log('Execution time: %dms', new Date() - start);
           return data[i] * data[j] * data[k]
         }
       }
--- template/day.js
+++ template/day.js
@@ -8,8 +8,7 @@ const part1 = input => {
   const start = new Date()
   const data = input.split("\n").map(Number);
 
-  const end = new Date() - start;
-  console.log('Execution time: %dms', end);
+  console.log('Execution time: %dms', new Date() - start);
   return input
 }
 
@@ -22,8 +21,7 @@ const part2 = input => {
   const start = new Date()
   const data = input.split("\n").map(Number);
 
-  const end = new Date() - start;
-  console.log('Execution time: %dms', end);
+  console.log('Execution time: %dms', new Date() - start);
   return input
 }
 
