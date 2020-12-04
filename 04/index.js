52c495bb5b8defd1d990a912060651ec7719b748
BuildTools <thies2003@gmail.com>
2020-12-04 13:26:12 +0100

    🧼 cleaned up code

--- 04/index.js
+++ 04/index.js
@@ -18,7 +18,7 @@ const part1 = input => {
   })
 
   const result = mapped.filter(x => {
-    const valid = true
+    let valid = true
     req.forEach(y => {
       if (x.indexOf(y) === -1) {
         valid = false
@@ -37,7 +37,7 @@ const part1 = input => {
 // ======
 // ~3.3 ms - answer: 127
 
-const req2 = {
+const validators = {
   "byr": { min: 1920, max: 2002 },
   "iyr": { min: 2010, max: 2020 },
   "eyr": { min: 2020, max: 2030 },
@@ -57,6 +57,7 @@ const part2 = input => {
   })
 
   const mapped = data.map(str => {
+    //Map the passport to an object with all property's
     return str.split(" ").reduce((acc, val) => {
       const test = val.split(":")
       acc[test[0]] = test[1]
@@ -66,50 +67,41 @@ const part2 = input => {
 
   const result = mapped.filter(current => {
     let valid = true
-    req.forEach(y => {
-      if (!current[y]) {
+    req.forEach(requirement => {
+      if (!current[requirement]) { //Property exists?
         valid = false
         return
       }
-      const z = req2[y]
+      const validator = validators[requirement]
 
-      if (z.special) {
-        if (y === "hgt") {
-          if (!(current[y].indexOf('in') > -1 || current[y].indexOf('cm') > -1)) {
+      if (validator.special) {
+        if (requirement === "hgt") { //Parse the height value
+          if (!(current[requirement].indexOf('in') > -1 || current[requirement].indexOf('cm') > -1)) {
             valid = false
             return
           }
-          const type = current[y].indexOf('in') > -1 ? "in" : "cm"
-          const str = current[y].substring(0, current[y].indexOf(type))
+          const type = current[requirement].indexOf('in') > -1 ? "in" : "cm"
+          const str = current[requirement].substring(0, current[requirement].indexOf(type))
 
           const val = parseInt(str);
-          if (val < z[type].min || val > z[type].max) {
+          if (val < validator[type].min || val > validator[type].max) {
             valid = false
             return
           }
-        } else if (y === "ecl") {
-          if (z.contains.indexOf(current[y]) === -1) {
-            valid = false;
-            return
-          }
-        } else if (y === "hcl") {
-          if (current[y][0] !== "#") {
-            valid = false
-            return
-          }
-          if (current[y].length !== 7 || !current[y].match(reggie)) {
-            valid = false
-            return
-          }
-        } else if (y === "pid") {
-          if (current[y].length !== 9 || parseInt(current[y].length) == NaN) {
-            valid = false;
-            return
-          }
+        } else if (requirement === "ecl" && (validator.contains.indexOf(current[requirement]) === -1)) { //Check if eye color is in the list
+          valid = false;
+          return
+        } else if (requirement === "hcl" && (current[requirement].length !== 7 || current[requirement][0] !== "#" || !current[requirement].match(reggie))) { //Check the hex color using a regex`, but first check length and first char
+          valid = false
+          return
+
+        } else if (requirement === "pid" && (current[requirement].length !== 9 || parseInt(current[requirement].length) == NaN)) {
+          valid = false;
+          return
         }
       } else {
-        const val = parseInt(current[y])
-        if (current[y].length !== 4 || val < z.min || val > z.max) {
+        const val = parseInt(current[requirement])
+        if (current[requirement].length !== 4 || val < validator.min || val > validator.max) {
           valid = false
           return
         }
