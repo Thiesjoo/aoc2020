059011bcfbce96e542217ea26b2a47ae5c40dd3e
BuildTools <thies2003@gmail.com>
2020-12-09 14:14:04 +0100

    🎆 yoinked code and optimized some stuff. Now also including first part in second part

--- 09/index.js
+++ 09/index.js
@@ -4,28 +4,21 @@ const now = require("performance-now")
 const preambleLength = 25
 // Part 1
 // ======
-// ~7.5 ms - answer: 15690279
+// ~2.7 ms - answer: 15690279
 
 const part1 = input => {
 	const start = now()
 	const data = input.split("\n").map(Number);
 
 	for (let i = preambleLength; i < data.length; i += 1) {
-		const element = data[i];
+		const sum = data[i];
 		const preamble = data.slice(i - preambleLength, i)
 
-		let sums = preamble.reduce((acc, val) => {
-
-			preamble.forEach(x => {
-				acc.push(x + val)
-			})
-			return acc
-		}, [])
-		if (!sums.includes(element)) {
+		if (!preamble.some((x) => preamble.some((y) => x + y === sum))) {
 			const end = now()
 			console.log('Execution time: ~%dms', (end - start).toFixed(3));
 
-			return element
+			return sum
 		}
 	}
 	return "NOT FOUND"
@@ -33,14 +26,16 @@ const part1 = input => {
 
 // Part 2
 // ======
-// ~2.2 ms - answer: 2174232
+// ~5.5 ms - answer: 2174232
+
 let maxLength = 2000
-const prevAnswer = 15690279
 
 const part2 = input => {
 	const start = now()
 	const data = input.split("\n").map(Number);
 	maxLength = Math.min(data.length, maxLength)
+
+	const prevAnswer = part1(input)
 
 	for (let i = 0; i < data.length; i++) {
 		let sum = data[i];
