58d0bca43964ecfcd5b209d2abe90400ee65a644
Thies Nieborg <thies2003@gmail.com>
2020-12-25 14:54:12 +0100

    👻 my own input now

--- 01/index.js
+++ 01/index.js
@@ -1,19 +1,20 @@
 "use strict";
-const now = require("performance-now")
+const now = require("performance-now");
+const toCheck = 2020;
 
 // Part 1
 // ======
 // 1.3 ms - answer: 703131
 
 const part1 = (input) => {
-  const start = now()
+  const start = now();
   const data = input.split("\n").map(Number);
   for (let i = 0; i < data.length; i++) {
     const element = data[i];
     for (let j = 0; j < data.length; j++) {
       const element1 = data[j];
-      if (element1 + element === 2020) {
-        console.log('Execution time: %dms', (now() - start).toFixed(3));
+      if (element1 + element === toCheck) {
+        console.log("Execution time: %dms", (now() - start).toFixed(3));
         return element * element1;
       }
     }
@@ -25,15 +26,15 @@ const part1 = (input) => {
 // 6.8 ms - answer: 272423970
 
 const part2 = (input) => {
-  const start = now()
+  const start = now();
   const data = input.split("\n").map(Number);
 
   for (let i = 0; i < data.length; i++) {
     for (let j = 0; j < data.length; j++) {
       for (let k = 0; k < data.length; k++) {
-        if (data[i] + data[j] + data[k] === 2020) {
-          console.log('Execution time: %dms', (now() - start).toFixed(3));
-          return data[i] * data[j] * data[k]
+        if (data[i] + data[j] + data[k] === toCheck) {
+          console.log("Execution time: %dms", (now() - start).toFixed(3));
+          return data[i] * data[j] * data[k];
         }
       }
     }
